f3c4c0fd2bb7e7daf5f012679bbb6eec46e97554
hemdarweb <hemdarweb@gmail.com>
2023-06-03 12:46:09 +0530

    delete post folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div style={{background:'red',textAlign:'center'}}>
+    <div >
       <Link href='/posts'>
         <h1>
           Post
